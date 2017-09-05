ba873b90169c8134bbc8ea754dd5df03bfe4a3de
Julien Cheype <cheype@gmail.com>
2017-09-05 17:22:43 +0200

    add services export

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export * from './src/app/modules/discovery/discovery.module';
+export {DiscoveryService, DiscoveryServiceConfig, DISCOVERY_SERVICE_CONFIG, Service} from './src/app/modules/discovery/discovery.service';
+export {DiscoveryDirective} from './src/app/modules/discovery/discovery.directive';
