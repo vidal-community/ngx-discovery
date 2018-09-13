70d0186cf37b893c29065f08e1a4e5e471789415
Fabien HIEGEL <fabien.hiegel@gmail.com>
2018-09-13 15:45:01 +0200

    chore: Clean up ngx-discovery

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/discovery/discovery.module';
-export {DiscoveryService, DiscoveryServiceConfig, DISCOVERY_SERVICE_CONFIG, Service} from './src/discovery/discovery.service';
-export {DiscoveryDirective} from './src/discovery/discovery.directive';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './discovery/discovery.module';
+export {DiscoveryService, DiscoveryServiceConfig, DISCOVERY_SERVICE_CONFIG, Service} from './discovery/discovery.service';
+export {DiscoveryDirective} from './discovery/discovery.directive';
